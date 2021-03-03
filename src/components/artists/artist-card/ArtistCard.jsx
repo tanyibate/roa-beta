0192edf256c81522460c4f32c68ce9c9e0f94725
Batetambe Tanyi <bate.tanyi@yahoo.com>
2021-03-03 17:57:27 +0000

    login-page

---
+++ src/components/artists/artist-card/ArtistCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function ArtistCard(props) {
+  return (
+    <div>
+      <div className="artist-card-top">
+        <div className="artist-card-image-container">
+          <img src="" alt="" />
+          <div className="artist-social-media"></div>
+        </div>
+        <div className="artist-card-bio"></div>
+      </div>
+      <div className="artist-card-bottom"></div>
+    </div>
+  );
+}
