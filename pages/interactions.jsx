c3a83a501de9c217f33ec7fc2c1703a851b6f280
Batetambe Tanyi <bate.tanyi@yahoo.com>
2021-03-28 20:47:01 +0100

    made changes to pagination arrow

--- pages/interactions.jsx
+++ pages/interactions.jsx
@@ -8,36 +8,43 @@ SwiperCore.use([Navigation, Pagination]);
 export default function interactions() {
   return (
     <div className={styles.interactions_container}>
-      <Swiper
-        navigation
-        pagination={{ clickable: true }}
-        breakpoints={{
-          340: {
-            slidesPerView: 1,
-            spaceBetween: 25,
-          },
-          920: { slidesPerView: 2, spaceBetween: 25 },
-          1240: {
-            slidesPerView: 3,
-            spaceBetween: 25,
-          },
-        }}
-        style={{
-          margin: "0px",
-          minWidth: "100%",
-          height: "425px",
-        }}
-      >
-        <SwiperSlide>
-          <Interaction />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Interaction />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Interaction />
-        </SwiperSlide>
-      </Swiper>
+      <div className={styles.swiper_container}>
+        <Swiper
+          navigation
+          pagination={{ clickable: true }}
+          breakpoints={{
+            340: {
+              slidesPerView: 1,
+              spaceBetween: 25,
+            },
+            920: { slidesPerView: 2, spaceBetween: 25 },
+            1240: {
+              slidesPerView: 3,
+              spaceBetween: 25,
+            },
+          }}
+          style={{
+            margin: "0px",
+            minWidth: "100%",
+            height: "425px",
+          }}
+        >
+          <SwiperSlide>
+            <Interaction />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Interaction />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Interaction />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className={styles.swiper_container_large}>
+        <Interaction />
+        <Interaction />
+        <Interaction />
+      </div>
     </div>
   );
 }
